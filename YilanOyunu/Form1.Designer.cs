665f4b50e7cd15b1b44514ef5cf6e391c8009cbb
Hamza Kilic <hamza2358@hotmail.com>
2022-12-03 17:51:27 +0300

    Proje dosyası ekle.

---
+++ YilanOyunu/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace YilanOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(12, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(612, 487);
+            this.panel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(644, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gold;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Brown;
+            this.button1.Location = new System.Drawing.Point(647, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Yeniden Başla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(769, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 34);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(815, 533);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(550, 420);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ YilanOyunu/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YilanOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        yilan yilanimiz = new yilan();
+        Yon yonumuz;
+        PictureBox[] pb_yilanparcalari;
+        bool yem_varmi = false;
+        Random rnd = new Random();
+        PictureBox pb_yem;
+        int skor = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            yeni_oyun();
+        }
+
+        private void yeni_oyun()
+        {
+            yilanimiz = new yilan();
+            yonumuz = new Yon(-10, 0);
+            pb_yilanparcalari = new PictureBox[0];
+            for (int i = 0; i < 3; i++)
+            {
+                Array.Resize(ref pb_yilanparcalari, pb_yilanparcalari.Length + 1);
+                pb_yilanparcalari[i] = Pb_ekle();
+            }
+            timer1.Start();
+            button1.Enabled = false;
+        }
+
+        private PictureBox Pb_ekle()
+        {       
+            PictureBox pb = new PictureBox();
+            pb.Size = new Size(10, 10); ;
+            pb.BackColor = Color.GreenYellow;
+            pb.Location = yilanimiz.GetPos(pb_yilanparcalari.Length-1);
+            panel1.Controls.Add(pb);
+            return pb;
+        }
+
+        private void Pb_guncelle()
+        {
+            for (int i = 0; i < pb_yilanparcalari.Length; i++)
+            {
+                pb_yilanparcalari[i].Location=yilanimiz.GetPos(i);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Up || e.KeyCode == Keys.W)
+            {
+                if (yonumuz._y != 10)
+                {
+                    yonumuz = new Yon(0, -10);
+                }
+            }
+            else if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                if (yonumuz._y != -10)
+                {
+                    yonumuz = new Yon(0, 10);
+                }
+            }
+            else if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                if (yonumuz._x != 10)
+                {
+                    yonumuz = new Yon(-10, 0);
+                }
+            }
+            else if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                if (yonumuz._x != -10)
+                {
+                    yonumuz = new Yon(10, 0);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = "Skor = " + skor.ToString();
+            yilanimiz.ilerle(yonumuz);
+            Pb_guncelle();
+            Yem_olustur();
+            Yem_yedi();
+            yandın();
+            Duvar_carpti();
+        }
+        public void Yem_olustur()
+        {
+            if (!yem_varmi)
+            {
+                PictureBox pb = new PictureBox();
+                pb.BackColor = Color.Red;
+                pb.Size = new Size(10, 10);
+                pb.Location = new Point(rnd.Next(panel1.Width / 10) * 10, rnd.Next(panel1.Height / 10) * 10);
+                pb_yem = pb;
+                yem_varmi = true;
+                panel1.Controls.Add(pb);
+            }
+        }
+        public void Yem_yedi()
+        {
+            if (yilanimiz.GetPos(0) == pb_yem.Location)
+            {
+                skor += 10;
+                yilanimiz.Buyu();
+                Array.Resize(ref pb_yilanparcalari, pb_yilanparcalari.Length + 1);
+                pb_yilanparcalari[pb_yilanparcalari.Length - 1] = Pb_ekle();
+                yem_varmi = false;
+                panel1.Controls.Remove(pb_yem);
+            }
+        }
+
+        public void yandın()
+        {
+            for (int i = 1; i < yilanimiz.Yilan_buyuklugu; i++)
+            {
+                if (yilanimiz.GetPos(0) == yilanimiz.GetPos(i))
+                {
+                    Yenildi();
+                }
+            }
+        }
+        public void Duvar_carpti()
+        {
+            Point p = yilanimiz.GetPos(0);
+            if(p.X < 0 || p.X > panel1.Width -10 || p.Y < 0 || p.Y > panel1.Height - 10)
+            {
+                Yenildi();
+            }
+        }
+        
+        private void Yenildi()
+        {
+            timer1.Stop();
+            MessageBox.Show("Oyun Bitti");
+            button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string puan = label1.Text;
+            PictureBox pb = new PictureBox();
+            pb.BackColor = Color.Red;
+            pb.Size = new Size(10, 10);
+            pb.Location = new Point(rnd.Next(panel1.Width / 10) * 10, rnd.Next(panel1.Height / 10) * 10);
+            skor = 0;
+            pb_yem = pb;
+            panel1.Controls.Clear();
+            panel1.Controls.Add(pb);
+            yeni_oyun();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
